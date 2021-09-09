9440753bc7f3a3e76a6d8f8c0e70439e1f157a1d
MarceloChoqueCuellar <90270295+MarceloChoqueCuellar@users.noreply.github.com>
2021-09-08 20:13:31 -0400

    CLASE 2

--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -1,3 +1,5 @@
-import pygame
-
-print("testing commit")
+from nlc_dino_runner.components.game import Game
+if __name__ == "__main__":
+    print("testing commit")
+    game = Game()
+    game.run()
